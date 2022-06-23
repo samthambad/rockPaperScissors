474073800f7e8c9cef670b0d4d139506e482a20f
ivysam-cmyk <98635284+ivysam-cmyk@users.noreply.github.com>
2022-06-23 17:34:31 +0800

    Update rockps.js

--- rockps.js
+++ rockps.js
@@ -1,10 +1,10 @@
 function computerPlay() {
-    randNum = Math.floor(Math.random()*3)
+    randNum = Math.floor(Math.random()*3)//gives between 0 and 1 then  *3 gives between 0 and 3 then flooring it gives 0,1,2
     const rpsList = ["rock", "paper", "scissors"]
     let computerChoice = rpsList[randNum]
     console.log("The computer chooses " + computerChoice)
     return computerChoice
-        
+
 }
 
 
@@ -35,14 +35,14 @@ function computerPlay() {
 
  
 function game() {
-    for (i = 0; i<5; i++) {
+    for (i = 0; i<5; i++) {//5 rounds
  
     let playerSelection = prompt("Rock, Paper, or Scissors?" , '');
     if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
         console.log("You choose "+ playerSelection)
         let computerSelection = computerPlay();
         playRound(playerSelection, computerSelection);//playRound is called here 
-        console.log("Round " + (i+1) + " complete")
+        console.log("Round " + (i+1) + " complete")// i+1 because i starts from 0
         }
     else 
         playerSelection = prompt("Please choose rock, paper, or scissors ", '');// for this if there is some random value for playerSelection the round will skip
